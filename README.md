4fadda4591699f3bb1228f5d43dbe3f1d29f29d1
jbpark <jbpark@wemakeprice.com>
2018-09-13 14:42:58 +0900

    update sample component & readme.md

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # react-dnd-component
 
->
+> components using react & react-dnd
 
 [![NPM](https://img.shields.io/npm/v/react-dnd-component.svg)](https://www.npmjs.com/package/react-dnd-component) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
@@ -12,16 +12,77 @@ npm install --save react-dnd-component
 
 ## Usage
 
+### List
+
 ```tsx
-import * as React from 'react';
+import React, { Component } from 'react';
+import { List, DragDropContext, HTML5Backend } from 'react-dnd-component';
 
-import MyComponent from 'react-dnd-component';
-
-class Example extends React.Component {
+class App extends Component {
   render() {
-    return <MyComponent />;
+    const style = {
+      display: 'flex',
+      justifyContent: 'space-around',
+      paddingTop: '20px'
+    };
+
+    const listStyle = {
+      width: 200,
+      height: 200
+    };
+
+    const itemStyle = {
+      width: 100,
+      height: 100,
+      display: 'inline-flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      color: 'white',
+      fontSize: 20
+    };
+
+    return (
+      <div style={{ ...style }}>
+        <List
+          name={'test1'}
+          id={1}
+          {...listStyle}
+          style={{
+            background: 'yellow'
+          }}
+          activeStyle={{
+            background: 'red'
+          }}
+        >
+          <div style={{ ...itemStyle, background: 'green' }}>test1</div>
+          <div style={{ ...itemStyle, background: 'green' }}>test2</div>
+          <div style={{ ...itemStyle, background: 'green' }}>test3</div>
+          <div style={{ ...itemStyle, background: 'green' }}>test4</div>
+          <div style={{ ...itemStyle, background: 'green' }}>test5</div>
+        </List>
+        <List
+          name={'test1'}
+          id={2}
+          {...listStyle}
+          style={{
+            background: 'orange'
+          }}
+          activeStyle={{
+            background: 'blue'
+          }}
+        >
+          <div style={{ ...itemStyle, background: 'gray' }}>test6</div>
+          <div style={{ ...itemStyle, background: 'gray' }}>test7</div>
+          <div style={{ ...itemStyle, background: 'gray' }}>test8</div>
+          <div style={{ ...itemStyle, background: 'gray' }}>test9</div>
+          <div style={{ ...itemStyle, background: 'gray' }}>test10</div>
+        </List>
+      </div>
+    );
   }
 }
+
+export default DragDropContext(HTML5Backend)(App);
 ```
 
 ## License
--- example/src/App.js
+++ example/src/App.js
@@ -9,13 +9,27 @@ class App extends Component {
       paddingTop: '20px'
     };
 
+    const listStyle = {
+      width: 200,
+      height: 200
+    };
+
+    const itemStyle = {
+      width: 100,
+      height: 100,
+      display: 'inline-flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      color: 'white',
+      fontSize: 20
+    };
+
     return (
       <div style={{ ...style }}>
         <List
           name={'test1'}
           id={1}
-          width={200}
-          height={200}
+          {...listStyle}
           style={{
             background: 'yellow'
           }}
@@ -23,43 +37,29 @@ class App extends Component {
             background: 'red'
           }}
         >
-          <div style={{ width: 100, height: 100 }}>test1</div>
-          <div style={{ width: 100, height: 100 }}>test2</div>
-          <div style={{ width: 100, height: 100 }}>test3</div>
-          <div style={{ width: 100, height: 100 }}>test4</div>
-          <div style={{ width: 100, height: 100 }}>test5</div>
-          <div style={{ width: 100, height: 100 }}>test6</div>
-          <div style={{ width: 100, height: 100 }}>test1</div>
-          <div style={{ width: 100, height: 100 }}>test2</div>
-          <div style={{ width: 100, height: 100 }}>test3</div>
-          <div style={{ width: 100, height: 100 }}>test4</div>
-          <div style={{ width: 100, height: 100 }}>test5</div>
-          <div style={{ width: 100, height: 100 }}>test6</div>
-          <div style={{ width: 100, height: 100 }}>test1</div>
-          <div style={{ width: 100, height: 100 }}>test2</div>
-          <div style={{ width: 100, height: 100 }}>test3</div>
-          <div style={{ width: 100, height: 100 }}>test4</div>
-          <div style={{ width: 100, height: 100 }}>test5</div>
-          <div style={{ width: 100, height: 100 }}>test6</div>
+          <div style={{ ...itemStyle, background: 'green' }}>test1</div>
+          <div style={{ ...itemStyle, background: 'green' }}>test2</div>
+          <div style={{ ...itemStyle, background: 'green' }}>test3</div>
+          <div style={{ ...itemStyle, background: 'green' }}>test4</div>
+          <div style={{ ...itemStyle, background: 'green' }}>test5</div>
         </List>
         <List
           name={'test1'}
           id={2}
-          width={200}
-          height={200}
+          {...listStyle}
+          style={{
+            background: 'orange'
+          }}
           activeStyle={{
             background: 'blue'
           }}
         >
-          <div>test7</div>
-          <div>test8</div>
-          <div>test9</div>
-          <div>test10</div>
-          <div>test11</div>
-          <div>test12</div>
+          <div style={{ ...itemStyle, background: 'gray' }}>test6</div>
+          <div style={{ ...itemStyle, background: 'gray' }}>test7</div>
+          <div style={{ ...itemStyle, background: 'gray' }}>test8</div>
+          <div style={{ ...itemStyle, background: 'gray' }}>test9</div>
+          <div style={{ ...itemStyle, background: 'gray' }}>test10</div>
         </List>
-        {/* <List name={'test2'} id={3} />
-        <List name={'test2'} id={4} /> */}
       </div>
     );
   }
